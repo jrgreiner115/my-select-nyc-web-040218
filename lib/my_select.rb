80ab32b71d17538d11d45f28b3bfae6c325c75be
Joe Greiner <jrgreiner115@gmail.com>
2018-03-27 15:10:22 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ if collection.length == 0
+return nil
+else
+  i = 0
+ new_array = []
+ while i < collection.length
+  if collection[i] % 2 == 0
+    new_array << yield(collection[i])
+  i += 1 
+end
+end
+new_array
+end
 end
